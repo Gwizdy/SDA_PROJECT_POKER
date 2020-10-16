882164fa61cca49319e0c73f26495c7cc37015c6
gwd <gwizdy@gmail.com>
2020-10-16 20:25:01 +0200

    oknogracze

--- src/com/taliakart/Main.java
+++ src/com/taliakart/Main.java
@@ -2,8 +2,7 @@ package com.taliakart;
 
 //Main do sprawdzania czy wszystko działa jak należy
 
-import com.okna.OknoPowitalne;
-import com.okna.Ramka;
+import com.okna.*;
 
 public class Main {
     public static void main(String[] args) {
